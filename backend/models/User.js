31b522a42cd5a6b5be11cfa16f2484f8c3c2f14e
Shinichi <miyakawashi@gmail.com>
2022-11-20 20:43:39 -0500

    update incorrect file update

---
+++ backend/models/User.js
@@ -0,0 +1,22 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  socketID: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: false,
+  },
+  rooms: [{ type: Schema.Types.ObjectId, ref: "Room" }],
+  messages: [{ type: Schema.Types.ObjectId, ref: "Message" }],
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
